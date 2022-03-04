ef57b026f2f8601418eabc2eeb84574ffde3866a
Parshukov Alexandr <parshukovae@yandex.ru>
2022-03-04 21:07:39 +0300

    работа с валидацией

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,21 +1,39 @@
-const checkVallidity = (formElement, inputElement) => {
-    console.log(inputElement.validity);
+const showError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.textContent = errorMessage;
+    
 }
+
+const hideError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.textContent = " ";
+}
+
+const checkValidity = (formElement, inputElement) => {
+    const isInputNotValidity = !inputElement.validity.valid;
+
+    if (isInputNotValidity) {
+        const errorMessage = inputElement.validationMessage;
+        showError(formElement, inputElement, errorMessage);
+    } else {
+        hideError(formElement, inputElement);
+    }
+};
 
 const setEventListeners = (formElement) => {
     const inputList = formElement.querySelectorAll('.popup__item');
     inputList.forEach((inputElement) => {
         inputElement.addEventListener("input", (evt) => {
             console.log(evt.target.name, evt.target.value)
-            checkVallidity(formElement, inputElement);
+            checkValidity(formElement, inputElement);
         });
     });
 };
 
 const enableValidation = () => {
     const formList = document.querySelectorAll(".popup__input-container");
-    formList.forEach((formElement) =>{
-        formElement.addEventListener("submit",  (evt) => {
+    formList.forEach((formElement) => {
+        formElement.addEventListener("submit", (evt) => {
             evt.preventDefault();
         });
         setEventListeners(formElement);
