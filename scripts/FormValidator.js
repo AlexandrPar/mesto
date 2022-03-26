b4ad50fadd4e9be84c12f4bd7b9f468de38f0ba1
Parshukov Alexandr <parshukovae@yandex.ru>
2022-03-26 09:48:52 +0300

    исправил после второго ревью

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,17 +1,17 @@
 export default class FormValidator {
-    constructor(config, formClass) {
+    constructor(config, form) {
         this._config = config;
-        this._formClass = formClass;
+        this._form = form;
     };
 
     _showError(inputElement) {
-        this._errorElement = this._formClass.querySelector(`.${inputElement.id}-error`);
+        this._errorElement = this._form.querySelector(`.${inputElement.id}-error`);
         inputElement.classList.add(this._config.inputErrorClass);
         this._errorElement.textContent = inputElement.validationMessage;
     };
 
     _hideError(inputElement) {
-        this._errorElement = this._formClass.querySelector(`.${inputElement.id}-error`);
+        this._errorElement = this._form.querySelector(`.${inputElement.id}-error`);
         inputElement.classList.remove(this._config.inputErrorClass);
         this._errorElement.textContent = "";
     };
@@ -53,8 +53,8 @@ export default class FormValidator {
     };
 
     _setEventListeners() {
-        this._inputList = Array.from(this._formClass.querySelectorAll(this._config.inputSelector));
-        this._buttonElement = this._formClass.querySelector(this._config.submitButtonSelector);
+        this._inputList = Array.from(this._form.querySelectorAll(this._config.inputSelector));
+        this._buttonElement = this._form.querySelector(this._config.submitButtonSelector);
         this._inputList.forEach(inputElement => {
             inputElement.addEventListener("input", () => {
                 this._checkValidity(inputElement);
@@ -65,7 +65,7 @@ export default class FormValidator {
 
     enableValidation = () => {
 
-        this._formClass.addEventListener("submit", (evt) => {
+        this._form.addEventListener("submit", (evt) => {
             evt.preventDefault();
         });
         this._setEventListeners();
